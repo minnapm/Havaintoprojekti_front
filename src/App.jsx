e94f3971db53308fcb2bc3ebfc86b309b19d2ca8
Minna Palm <mppalm@utu.fi>
2024-07-01 14:52:57 +0300

    lisätty ilmoitus, kirjautuminen

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Observation from './components/Observation'
 import ObservationForm from './components/ObservationForm'
-import observationService from './services/observations'
+import obsService from './services/observations'
+import loginService from './services/login'
+import Togglable from './components/Togglable'
+
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className="message">
+      {message}
+    </div>
+  )
+}
 
 const Observations = (props) => {
+  console.log(props.observations)
   return (
     <ul>
       {props.observations.map(observation =>
@@ -17,14 +32,30 @@ const Observations = (props) => {
 
 const App = () => {
   const [observations, setObservations] = useState([]) 
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+  const [newMessage, setNewMessage] = useState('')
+
+  const obsFormRef = useRef()
 
   useEffect(() => {
-    observationService
+    obsService
     .getAll()
     .then(observations => {
+      //console.log("Observations from api: " + observations)
       setObservations(observations)
     })
-  })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedObservationappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      obsService.setToken(user.token)
+    }
+  }, [])
 
   const menuLines = document.querySelector('.menu-lines')
   const nav = document.querySelector('.nav-menu')
@@ -34,21 +65,88 @@ const App = () => {
     nav.classList.toggle('hide')
   })
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedObservationappUser', JSON.stringify(user)
+      )
+      obsService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setNewMessage('wrong username or password')
+      setTimeout(() => {
+        setNewMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedObservationappUser')
+  }
+
   const addObservation = (observationObject) =>{
-    console.log("havainnon lisäämisnappia painettiin", event.target)
-    blogService
+    console.log("havainnon lisäämisnappia painettiin")
+    obsFormRef.current.toggleVisibility()
+    obsService
     .create(observationObject)
-    .then(returnedObservation => {
-      setObservations(observations.concat(returnedObservation))
+    .then(returnedObs => {
+      setObservations(observations.concat(returnedObs))
+      setNewMessage(`Lisättiin uusi havainto: ${observationObject.species}`)
+        setTimeout( () => {
+          setNewMessage(null)
+        }, 3000)
     })
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Kirjaudu sisään</h2>
+        <Notification message={newMessage} />
+        <form onSubmit = {handleLogin}>
+          <div>
+            käyttäjänimi
+            <input 
+            type="text" 
+            value={username} 
+            name="Username"
+            onChange = {({ target }) => setUsername(target.value)} />
+          </div>
+          <div>
+            salasana
+            <input 
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)} />
+          </div>
+          <button type="submit">Kirjaudu</button>
+        </form>
+      </div>
+    )
   }
 
 
   return (
   <div>
     <h2>Havaintopäiväkirja</h2>
-    <Observations observations={observations} />
-    <ObservationForm createObservation={addObservation} />
+    <Notification message={newMessage} />
+      <form onSubmit = {handleLogout}>
+        <div>
+        <p>{user.name} kirjautunut sisääb</p>
+        <button type="submit">kirjaudu ulos</button>
+        </div>
+      </form>
+      <Togglable buttonLabel="Uusi havainto" ref={obsFormRef}>
+         <ObservationForm createObservation={addObservation} />
+     </Togglable>
+     <Observations observations={observations} />
   </div>
   )
 }
--- src/services/observations.js
+++ src/services/observations.js
@@ -1,24 +1,35 @@
 import axios from 'axios'
 
-
 const baseUrl = '/api/observations'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
 
 const getAll = () => {
-    return axios.get(baseUrl)
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
 }
 
-const create = newObject => {
-    return axios.post(baseUrl, newObject)
+const create = async newObject => {
+    const config = {
+        headers: { Authorization: token },
+      }
+    const response = await axios.post(baseUrl, newObject, config)
+    return response.data
 }
 
-const updateToDelete = (id) => {
-    return axios.delete(`${baseUrl}/${id}`)
+const updateToDelete = async (id) => {
+    const response = await axios.delete(`${baseUrl}/${id}`)
+    return response.data
 }
 
-const updateObservation = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject)
+const updateObservation = async (id, newObject) => {
+    const response = await axios.put(`${baseUrl}/${id}`, newObject)
+    return response.data
 }
 
 
-export default { getAll, create, updateToDelete, updateObservation }
+export default { getAll, create, updateToDelete, updateObservation, setToken }
