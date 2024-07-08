a77ba864120e348226f8c7799b64b42a4400c9d0
Minna Palm <mppalm@utu.fi>
2024-07-08 16:59:33 +0300

    lisätty kategoria havaintolomakkeeseen

--- src/components/ObservationForm.jsx
+++ src/components/ObservationForm.jsx
@@ -5,6 +5,7 @@ const ObservationForm = ({ createObservation }) => {
     const [newAmount, setNewAmount] = useState('')
     const [newPlace, setNewPlace] = useState('')
     const [newDate, setNewDate] = useState('')
+    const [category, setCategory] = useState('')
 
     const addObservation = (event) =>{
         event.preventDefault()
@@ -13,12 +14,14 @@ const ObservationForm = ({ createObservation }) => {
             amount: newAmount,
             place: newPlace,
             date: newDate,
+            category: category,
         })
 
         setNewSpecies('')
         setNewAmount('')
         setNewPlace('')
         setNewDate('')
+        setCategory('')
     }
 
     return (
@@ -53,6 +56,37 @@ const ObservationForm = ({ createObservation }) => {
               className="observationinput"
               value={newDate}
               onChange = {event => setNewDate(event.target.value)} />
+            </div>
+            <div className="radio">
+              <p>kategoria:</p>
+              <input 
+              type="radio" 
+              name="category"
+              value="Kasvit"
+              checked={category === 'Kasvit'}
+              onChange={event => setCategory(event.target.value)}/>
+              Kasvit 
+              <input 
+              type="radio"
+              name="category" 
+              value="Sienet"
+              checked={category === 'Sienet'}
+              onChange={event => setCategory(event.target.value)}/>
+              Sienet 
+              <input 
+              type="radio" 
+              name="category"
+              checked={category === 'Linnut'}
+              value="Linnut"
+              onChange={event => setCategory(event.target.value)}/>
+              Linnut  
+              <input 
+              type="radio" 
+              name="category"
+              value="Perhoset"
+              checked={category === 'Perhoset'}
+              onChange={event => setCategory(event.target.value)}/>
+              Perhoset  
             </div>
             <button className="savebutton" type="submit">Tallenna</button>
           </form>
